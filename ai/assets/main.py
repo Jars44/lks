df99ed978f6eccddd380f1c7333a5462f46a6846
Jars <maulidfajar163@gmail.com>
2025-06-11 22:28:12 +0700

    `Update main.py to read CSV instead of Excel and handle missing values in oldpeak column.`

--- ai/assets/main.py
+++ ai/assets/main.py
@@ -4,5 +4,13 @@ import tkinter
 import matplotlib
 import seaborn
 
-df = pd.read_excel(r"O:\Tugas\lks\ai\assets\Datasset LKS AI Kabupaten Malang 2025.csv.xls")
+df = pd.read_csv(r"O:\Tugas\lks\ai\assets\Datasset LKS AI Kabupaten Malang 2025.csv.xls")
 df = df.drop_duplicates()
+# df["chol"] = df["chol"].split(',', 1, expand=True)
+# for x in df.index:
+#     if df.loc[x] == ' ':
+#         df.replace("missing")
+for x in df.index:
+    if df.loc[x,"oldpeak"] == '':
+        df.loc[x,"oldpeak"] = 0
+print(df)
