6d282765a3c8b8b59c8f71814837d3f04f3c442b
Jars <maulidfajar163@gmail.com>
2025-06-12 23:25:55 +0700

    `Refactor main.py: update import, add entropy calculation, remove unused code`

--- ai/assets/main.py
+++ ai/assets/main.py
@@ -3,13 +3,22 @@ import pandas as pd
 import tkinter as tk
 import matplotlib as mpl
 import seaborn as sbn
-
-def entropy():
-    df = pd.read_csv(r"O:\Tugas\lks\ai\assets\Datasset LKS AI Kabupaten Malang 2025.csv.xls")
-    n = np()
+from numpy import log2
 
 df = pd.read_csv(r"O:\Tugas\lks\ai\assets\Datasset LKS AI Kabupaten Malang 2025.csv.xls")
-df = df.drop_duplicates()
+# x = df.drop('label', axis=1)
+# y = df['label']
+
+total = 10
+pA = 8 / total
+pB = 2 / total
+entropy = - (pA * log2(pA) + pB * log2(pB))
+
+print(entropy)
+# def entropy():
+#     df = pd.read_csv(r"O:\Tugas\lks\ai\assets\Datasset LKS AI Kabupaten Malang 2025.csv.xls")
+#     n = np()
+
 
 # df["chol"] = df["chol"].split(',', 1, expand=True)
 # for x in df.index:
