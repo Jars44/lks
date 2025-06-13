a02b177b913017df5fa1e0a8bdbb039e14ac8b1a
Jars <maulidfajar163@gmail.com>
2025-06-13 21:18:38 +0700

    `Refactored main.py in ai/assets directory with changes to entropy calculation and data manipulation.`

--- ai/assets/main.py
+++ ai/assets/main.py
@@ -9,22 +9,34 @@ df = pd.read_csv(r"O:\Tugas\lks\ai\assets\Datasset LKS AI Kabupaten Malang 2025.
 # x = df.drop('label', axis=1)
 # y = df['label']
 
-total = 10
-pA = 8 / total
-pB = 2 / total
-entropy = - (pA * log2(pA) + pB * log2(pB))
+# total = 10
+# pA = 8 / total
+# pB = 2 / total
+# entropy = - (pA * log2(pA) + pB * log2(pB))
 
-print(entropy)
+# print(entropy)
 # def entropy():
 #     df = pd.read_csv(r"O:\Tugas\lks\ai\assets\Datasset LKS AI Kabupaten Malang 2025.csv.xls")
 #     n = np()
 
 
+age = df["age"]
+sex = df["sex"]
+sex = df["sex"]
+# sex = df["sex"].replace((1, "male"))
+# sex = df["sex"].replace((2, "female"))
+if df["sex"] == 1:
+    df["sex"].replace(1, "male")
+print(df)
+# print(df["sex"].replace(1, "male"))
+# print(df["sex"].replace(0, "female"))
+
 # df["chol"] = df["chol"].split(',', 1, expand=True)
 # for x in df.index:
 #     if df.loc[x] == ' ':
 #         df.replace("missing")
-for x in df.index:
-    if df.loc[x,"oldpeak"] == '':
-        df.loc[x,"oldpeak"] = 0
-print(df)
+
+# for x in df.index:
+#     if df.loc[x,"oldpeak"] == '':
+#         df.loc[x,"oldpeak"] = 0
+# print(df)
