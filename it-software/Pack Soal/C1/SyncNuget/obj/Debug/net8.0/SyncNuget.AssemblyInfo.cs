450e40f04c6f2fe374a1a84eeb39b3c407e2869b
Jars44 <maulidfajar163@gmail.com>
2025-08-04 08:09:26 +0700

    `Updated project directories and assembly information for SyncNuget, SyncNuget2, and TiksAPI projects.`

--- it-software/Pack Soal/C1/SyncNuget/obj/Debug/net8.0/SyncNuget.AssemblyInfo.cs
+++ it-software/Pack Soal/C1/SyncNuget/obj/Debug/net8.0/SyncNuget.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyncNuget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661beb6469a4fbd6a278f921ee07ff054830cfc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyncNuget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyncNuget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
