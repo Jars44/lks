e1c255561e9bea5918592afba0581ef3164fb336
Jars44 <maulidfajar163@gmail.com>
2025-08-06 21:44:23 +0700

    `Refactored HTML, CSS, and JavaScript files for the client media module.`

--- web-technology/MODULE_CLIENT_MEDIA/script.js
+++ web-technology/MODULE_CLIENT_MEDIA/script.js
@@ -1,12 +1,28 @@
-document.getElementById('home').style.display = 'block';
-document.getElementById('button-instruction').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('instruction').style.display = 'block';
-});
+document.getElementById("home").style.display = "block";
 
-document.getElementById('cross-instruction').addEventListener('click', function() {
-    document.getElementById('instruction').style.display = 'none';
-    document.getElementById('home').style.display = 'block';
-});
+function Play() {
+  var username = document.getElementById("username");
+  console.log(username.value);
+  var difficulty = document.getElementById("level");
+  console.log(difficulty.value);
 
+  if (username.value === "") {
+    return;
+  } else if (difficulty.value === "0") {
+    return;
+  } else {
+    document.getElementById("game").style.opacity = "100%";
+    document.getElementById("home").style.opacity = "0%";
+  }
+}
+
+function Instruction() {
+  document.getElementById("instruction").style.display = "block";
+  document.getElementById("home").style.display = "none";
+  // window.location.href = `game.html?difficulty=${difficulty}`;
+}
+
+function CloseInstruction() {
+  document.getElementById("instruction").style.display = "none";
+  document.getElementById("home").style.display = "block";
+}
